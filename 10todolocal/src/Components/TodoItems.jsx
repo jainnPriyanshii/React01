fbc7a5e552c24ee98c3991a712b96ff38b5e42ab
jainnpriyanshii <priyanshijain664@gmail.com>
2024-04-19 12:37:54 +0530

    Change

--- 10todolocal/src/Components/TodoItems.jsx
+++ 10todolocal/src/Components/TodoItems.jsx
@@ -32,7 +32,7 @@ function TodoItem({ todo }) {
                 type="text"
                 className={`border outline-none w-full bg-transparent rounded-lg ${
                     isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                } ${todo.completed? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
